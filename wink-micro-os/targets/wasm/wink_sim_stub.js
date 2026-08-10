3c34a51b518ccd8c19e623c05b163e66c193ddcd
ding gao <771747384@qq.com>
2026-08-10 23:41:45 +0800

    refactor(wasm): remove deprecated js_pal_gpio_read and ultrasonic distance APIs

--- wink-micro-os/targets/wasm/wink_sim_stub.js
+++ wink-micro-os/targets/wasm/wink_sim_stub.js
@@ -63,7 +63,6 @@ if (isMainThread) {
 
     const knownBridgeSymbols = [
         'js_pal_gpio_write',
-        'js_pal_gpio_read',
         'js_pal_adc_read_norm',
         'js_pal_gpio_read_state',
         'js_pal_gpio_drive_ideal',
