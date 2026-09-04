58c9de9de1ca57ef98e58fbdae9e0fce4fddee09
ding gao <771747384@qq.com>
2026-09-04 17:02:47 +0800

    build(rc_servo): 更新舵机画布组件防旋转内联样式产物

--- wink-plugin-peripherals/builtin/rc_servo/1.0.0/dist/frontend.js
+++ wink-plugin-peripherals/builtin/rc_servo/1.0.0/dist/frontend.js
@@ -1,9 +1,19 @@
 import { definePeripheral as e, pinsFromBinderVariant as t, resolvePluginInstanceId as n } from "@wink-ai/unisim-ui";
 import { resolvePluginIdentity as r } from "@wink-ai/unisim";
-import { computed as i, createElementBlock as a, createElementVNode as o, defineComponent as s, normalizeClass as c, openBlock as l, toDisplayString as u } from "vue";
+import { computed as i, createElementBlock as a, createElementVNode as o, defineComponent as s, normalizeClass as c, normalizeStyle as l, openBlock as u, toDisplayString as d } from "vue";
 import "@wokwi/elements";
 //#region builtin/rc_servo/1.0.0/src/CanvasGlyph.vue?vue&type=script&setup=true&lang.ts
-var d = { class: "servo-container" }, f = ["angle"], p = /*#__PURE__*/ ((e, t) => {
+var f = {
+	class: "servo-container",
+	style: {
+		position: "relative",
+		display: "flex",
+		"align-items": "center",
+		"justify-content": "center",
+		width: "100%",
+		height: "100%"
+	}
+}, p = ["angle"], m = /*#__PURE__*/ ((e, t) => {
 	let n = e.__vccOpts || e;
 	for (let [e, r] of t) n[e] = r;
 	return n;
@@ -17,12 +27,31 @@ var d = { class: "servo-container" }, f = ["angle"], p = /*#__PURE__*/ ((e, t) =
 		rotation: {}
 	},
 	setup(e) {
-		let t = e, n = i(() => Math.abs((t.rotation ?? 0) % 180) === 90);
-		return (t, r) => (l(), a("div", d, [o("wokwi-servo", { angle: e.angle }, null, 8, f), o("span", { class: c(["label", { "is-transposed": n.value }]) }, u(e.label || e.id) + " (" + u(Math.round(e.angle)) + "°) ", 3)]));
+		let t = e, n = i(() => Math.abs((t.rotation ?? 0) % 180) === 90), r = i(() => {
+			let e = n.value ? "48px" : "38px";
+			return {
+				position: "absolute",
+				left: "50%",
+				top: "50%",
+				transformOrigin: "0 0",
+				transform: `rotate(calc(-1 * var(--rot, ${t.rotation ?? 0}deg))) translateY(${e}) translateX(-50%)`,
+				fontSize: "9px",
+				color: "#94a3b8",
+				whiteSpace: "nowrap",
+				pointerEvents: "none",
+				userSelect: "none",
+				textAlign: "center",
+				transition: "transform 0.15s ease"
+			};
+		});
+		return (t, i) => (u(), a("div", f, [o("wokwi-servo", { angle: e.angle }, null, 8, p), o("span", {
+			class: c(["label", { "is-transposed": n.value }]),
+			style: l(r.value)
+		}, d(e.label || e.id) + " (" + d(Math.round(e.angle)) + "°) ", 7)]));
 	}
-}), [["__scopeId", "data-v-414a5e05"]]);
+}), [["__scopeId", "data-v-8d19946d"]]);
 Object.freeze({});
-var m = Object.freeze({
+var h = Object.freeze({
 	PWM: Object.freeze({
 		relX: -5,
 		relY: 50,
@@ -44,10 +73,10 @@ var m = Object.freeze({
 		defaultConnection: "GND",
 		required: !1
 	})
-}), h = Object.freeze({ sg90: Object.freeze({
+}), g = Object.freeze({ sg90: Object.freeze({
 	variant: "sg90",
 	getPins: () => t("rc_servo", "sg90"),
-	pinsOverlay: m,
+	pinsOverlay: h,
 	defaultAppearanceId: "rc_servo_sg90"
 }) });
 Object.freeze({ rc_servo_sg90: Object.freeze({
@@ -62,14 +91,14 @@ Object.freeze({ rc_servo_sg90: Object.freeze({
 }) });
 //#endregion
 //#region builtin/rc_servo/1.0.0/src/definition.ts
-var g = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), _ = h.sg90, v = e({
-	type: g.type,
+var _ = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), v = g.sg90, y = e({
+	type: _.type,
 	size: {
 		width: 80,
 		height: 60
 	},
 	wireColor: "#3b82f6",
-	pinsOverlay: _.pinsOverlay,
+	pinsOverlay: v.pinsOverlay,
 	props: {
 		variant: {
 			type: "string",
@@ -127,7 +156,7 @@ var g = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), _ = h.sg90, v = e({
 			}
 		}
 	},
-	canvas: p,
+	canvas: m,
 	ui: { canvasProps: (e, t) => {
 		let r = n(e, "rc_servo"), i = t.pluginChannels?.[r]?.angle, a = typeof i == "number" ? i : 90;
 		return {
@@ -140,4 +169,4 @@ var g = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), _ = h.sg90, v = e({
 	} }
 });
 //#endregion
-export { v as default, v as servoDefinition };
+export { y as default, y as servoDefinition };
