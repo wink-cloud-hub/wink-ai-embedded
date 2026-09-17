4fcc57d040339f1d456cb33c1de39d5f8c2d8965
ding gao <771747384@qq.com>
2026-09-17 23:31:48 +0800

    fix(peripheral/rc_servo): pass manifest and category to definePeripheral and align props schema

--- wink-plugin-peripherals/builtin/rc_servo/1.0.0/dist/frontend.js
+++ wink-plugin-peripherals/builtin/rc_servo/1.0.0/dist/frontend.js
@@ -1,9 +1,9 @@
 import { definePeripheral as e, pinsFromBinderVariant as t, resolvePluginInstanceId as n } from "@wink-ai/unisim-ui";
-import { resolvePluginIdentity as r } from "@wink-ai/unisim-sdk";
-import { computed as i, createElementBlock as a, createElementVNode as o, defineComponent as s, normalizeClass as c, normalizeStyle as l, openBlock as u, toDisplayString as d } from "vue";
+import { normalizeManifest as r, resolvePluginIdentity as i } from "@wink-ai/unisim-sdk";
+import { computed as a, createElementBlock as o, createElementVNode as s, defineComponent as c, normalizeClass as l, normalizeStyle as u, openBlock as d, toDisplayString as f } from "vue";
 import "@wokwi/elements";
 //#region builtin/rc_servo/1.0.0/src/CanvasGlyph.vue?vue&type=script&setup=true&lang.ts
-var f = {
+var p = {
 	class: "servo-container",
 	style: {
 		position: "relative",
@@ -13,11 +13,11 @@ var f = {
 		width: "100%",
 		height: "100%"
 	}
-}, p = ["angle"], m = /*#__PURE__*/ ((e, t) => {
+}, m = ["angle"], h = /*#__PURE__*/ ((e, t) => {
 	let n = e.__vccOpts || e;
 	for (let [e, r] of t) n[e] = r;
 	return n;
-})(/* @__PURE__ */ s({
+})(/* @__PURE__ */ c({
 	__name: "CanvasGlyph",
 	props: {
 		id: {},
@@ -27,7 +27,7 @@ var f = {
 		rotation: {}
 	},
 	setup(e) {
-		let t = e, n = i(() => Math.abs((t.rotation ?? 0) % 180) === 90), r = i(() => {
+		let t = e, n = a(() => Math.abs((t.rotation ?? 0) % 180) === 90), r = a(() => {
 			let e = n.value ? "48px" : "38px";
 			return {
 				position: "absolute",
@@ -44,15 +44,15 @@ var f = {
 				transition: "transform 0.15s ease"
 			};
 		});
-		return (t, i) => (u(), a("div", f, [o("wokwi-servo", { angle: e.angle }, null, 8, p), o("span", {
-			class: c(["label", { "is-transposed": n.value }]),
-			style: l(r.value)
-		}, d(e.label || e.id) + " (" + d(Math.round(e.angle)) + "°) ", 7)]));
+		return (t, i) => (d(), o("div", p, [s("wokwi-servo", { angle: e.angle }, null, 8, m), s("span", {
+			class: l(["label", { "is-transposed": n.value }]),
+			style: u(r.value)
+		}, f(e.label || e.id) + " (" + f(Math.round(e.angle)) + "°) ", 7)]));
 	}
-}), [["__scopeId", "data-v-8d19946d"]]), h = Object.freeze({
+}), [["__scopeId", "data-v-8d19946d"]]), g = Object.freeze({
 	width: 171,
 	height: 120
-}), g = Object.freeze({
+}), _ = Object.freeze({
 	GND: Object.freeze({
 		relX: 0,
 		relY: 50,
@@ -76,10 +76,10 @@ var f = {
 	})
 });
 Object.freeze({});
-var _ = g, v = h, y = Object.freeze({ sg90: Object.freeze({
+var v = _, y = g, b = Object.freeze({ sg90: Object.freeze({
 	variant: "sg90",
 	getPins: () => t("rc_servo", "sg90"),
-	pinsOverlay: _,
+	pinsOverlay: v,
 	defaultAppearanceId: "rc_servo_sg90"
 }) });
 Object.freeze({ rc_servo_sg90: Object.freeze({
@@ -93,13 +93,133 @@ Object.freeze({ rc_servo_sg90: Object.freeze({
 	])
 }) });
 //#endregion
+//#region builtin/rc_servo/1.0.0/src/simulation.ts
+var x = i(import.meta.url, "rc_servo", "1.0.0", "output"), S = { sg90: {
+	displayName: "RC Servo Motor",
+	pins: [
+		{
+			name: "PWM",
+			pinType: "pwm",
+			required: !0
+		},
+		{
+			name: "VCC",
+			pinType: "vcc",
+			voltage: "5V",
+			required: !1
+		},
+		{
+			name: "GND",
+			pinType: "gnd",
+			required: !1
+		}
+	]
+} };
+function C(e = "sg90") {
+	let t = S[e] ?? S.sg90;
+	return r({
+		type: x.type,
+		version: x.version,
+		category: x.category,
+		displayName: t.displayName,
+		description: "RC Servo Motor (0-180 degrees)",
+		timingModel: "event-driven",
+		pins: t.pins,
+		properties: {
+			variant: {
+				type: "string",
+				default: e
+			},
+			minAngle: {
+				type: "number",
+				default: 0,
+				min: 0,
+				max: 180
+			},
+			maxAngle: {
+				type: "number",
+				default: 180,
+				min: 0,
+				max: 180
+			},
+			minPulseMs: {
+				type: "number",
+				default: .5,
+				min: .3,
+				max: 1
+			},
+			maxPulseMs: {
+				type: "number",
+				default: 2.5,
+				min: 2,
+				max: 3
+			},
+			framePeriodMs: {
+				type: "number",
+				default: 20,
+				min: 1,
+				max: 100,
+				unit: "ms"
+			},
+			pwmChannel: {
+				type: "number",
+				default: 0,
+				min: 0,
+				max: 15
+			}
+		},
+		stateChannels: {
+			angle: {
+				type: "number",
+				default: 90,
+				unit: "degrees",
+				description: "Current servo angle"
+			},
+			targetAngle: {
+				type: "number",
+				default: 90,
+				unit: "degrees",
+				show: !1,
+				description: "Target angle (smoothing)"
+			}
+		},
+		events: { SET_ANGLE: {
+			description: "Set servo target angle",
+			params: { angle: {
+				type: "number",
+				required: !0,
+				unit: "degrees"
+			} }
+		} }
+	});
+}
+C("sg90");
+//#endregion
 //#region builtin/rc_servo/1.0.0/src/definition.ts
-var b = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), x = y.sg90, S = e({
-	type: b.type,
-	size: v,
+var w = i(import.meta.url, "rc_servo", "1.0.0", "actuator"), T = b.sg90, E = e({
+	type: w.type,
+	displayName: "RC Servo Motor",
+	category: "actuator",
+	manifest: C(),
+	size: y,
 	wireColor: "#3b82f6",
-	pinsOverlay: x.pinsOverlay,
+	pinsOverlay: T.pinsOverlay,
 	props: {
+		angle: {
+			type: "number",
+			default: 90,
+			description: "Current Angle (degrees)",
+			range: {
+				min: 0,
+				max: 180,
+				step: 1
+			}
+		},
+		invert: {
+			type: "boolean",
+			default: !1,
+			description: "Invert direction"
+		},
 		variant: {
 			type: "string",
 			default: "sg90",
@@ -156,7 +276,7 @@ var b = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), x = y.sg90, S = e({
 			}
 		}
 	},
-	canvas: m,
+	canvas: h,
 	ui: { canvasProps: (e, t) => {
 		let r = n(e, "rc_servo"), i = t.pluginChannels?.[r]?.angle, a = typeof i == "number" ? i : 90;
 		return {
@@ -169,4 +289,4 @@ var b = r(import.meta.url, "rc_servo", "1.0.0", "actuator"), x = y.sg90, S = e({
 	} }
 });
 //#endregion
-export { S as default, S as servoDefinition };
+export { E as default, E as servoDefinition };
