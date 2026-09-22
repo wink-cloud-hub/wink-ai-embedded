5e342b327380577b0ee59e6d4cc05b5cbe1a08c0
ding gao <771747384@qq.com>
2026-09-22 11:47:19 +0800

    chore(apps): rebuild I2C/SPI vendor assets for the new ABI hash
    Re-run 'winkcli build sim' for i2c_master_at24c256 and spi_master_95256 so the committed unisim-assets embed PAL_WASM_ABI_HASH 0x6B769176; the previous bundles would fail WasmPhysicalBridge.verifyAbiHash fail-fast.

--- wink-micro-app/vendor/cms8s78xx/i2c_master_at24c256/unisim-assets/wink_simulator.js
+++ wink-micro-app/vendor/cms8s78xx/i2c_master_at24c256/unisim-assets/wink_simulator.js
@@ -1651,6 +1651,13 @@ async function createWasm() {
           return 1;
       }
 
+  function _js_pal_i2c_transfer_ex(port, addr, wbuf, wlen, rbuf, rlen, resultPtr) {
+          if (typeof Module !== 'undefined' && typeof Module['js_pal_i2c_transfer_ex'] === 'function' && Module['js_pal_i2c_transfer_ex'] !== _js_pal_i2c_transfer_ex) {
+              return Module['js_pal_i2c_transfer_ex'](port, addr, wbuf, wlen, rbuf, rlen, resultPtr);
+          }
+          return -7;
+      }
+
   function _js_pal_log(level, msgPtr) {
           if (typeof Module !== 'undefined' && typeof Module['js_pal_log'] === 'function' && Module['js_pal_log'] !== _js_pal_log) {
               return Module['js_pal_log'](level, msgPtr);
@@ -2383,6 +2390,7 @@ var _pal_wasm_set_gpio_input = Module['_pal_wasm_set_gpio_input'] = makeInvalidE
 var _pal_wasm_get_gpio_output = Module['_pal_wasm_get_gpio_output'] = makeInvalidEarlyAccess('_pal_wasm_get_gpio_output');
 var _pal_wasm_get_prng_state = Module['_pal_wasm_get_prng_state'] = makeInvalidEarlyAccess('_pal_wasm_get_prng_state');
 var _pal_wasm_i2c_transfer = Module['_pal_wasm_i2c_transfer'] = makeInvalidEarlyAccess('_pal_wasm_i2c_transfer');
+var _pal_wasm_i2c_transfer_ex = Module['_pal_wasm_i2c_transfer_ex'] = makeInvalidEarlyAccess('_pal_wasm_i2c_transfer_ex');
 var _pal_wasm_push_uart_rx_byte = Module['_pal_wasm_push_uart_rx_byte'] = makeInvalidEarlyAccess('_pal_wasm_push_uart_rx_byte');
 var _pal_wasm_report_oom = Module['_pal_wasm_report_oom'] = makeInvalidEarlyAccess('_pal_wasm_report_oom');
 var _pal_wasm_push_uart_rx_error = Module['_pal_wasm_push_uart_rx_error'] = makeInvalidEarlyAccess('_pal_wasm_push_uart_rx_error');
@@ -2489,6 +2497,7 @@ function assignWasmExports(wasmExports) {
   assert(typeof wasmExports['pal_wasm_get_gpio_output'] != 'undefined', 'missing Wasm export: pal_wasm_get_gpio_output');
   assert(typeof wasmExports['pal_wasm_get_prng_state'] != 'undefined', 'missing Wasm export: pal_wasm_get_prng_state');
   assert(typeof wasmExports['pal_wasm_i2c_transfer'] != 'undefined', 'missing Wasm export: pal_wasm_i2c_transfer');
+  assert(typeof wasmExports['pal_wasm_i2c_transfer_ex'] != 'undefined', 'missing Wasm export: pal_wasm_i2c_transfer_ex');
   assert(typeof wasmExports['pal_wasm_push_uart_rx_byte'] != 'undefined', 'missing Wasm export: pal_wasm_push_uart_rx_byte');
   assert(typeof wasmExports['pal_wasm_report_oom'] != 'undefined', 'missing Wasm export: pal_wasm_report_oom');
   assert(typeof wasmExports['pal_wasm_push_uart_rx_error'] != 'undefined', 'missing Wasm export: pal_wasm_push_uart_rx_error');
@@ -2591,6 +2600,7 @@ function assignWasmExports(wasmExports) {
   _pal_wasm_get_gpio_output = Module['_pal_wasm_get_gpio_output'] = createExportWrapper('pal_wasm_get_gpio_output', wasmExports['pal_wasm_get_gpio_output'], 1);
   _pal_wasm_get_prng_state = Module['_pal_wasm_get_prng_state'] = createExportWrapper('pal_wasm_get_prng_state', wasmExports['pal_wasm_get_prng_state'], 0);
   _pal_wasm_i2c_transfer = Module['_pal_wasm_i2c_transfer'] = createExportWrapper('pal_wasm_i2c_transfer', wasmExports['pal_wasm_i2c_transfer'], 6);
+  _pal_wasm_i2c_transfer_ex = Module['_pal_wasm_i2c_transfer_ex'] = createExportWrapper('pal_wasm_i2c_transfer_ex', wasmExports['pal_wasm_i2c_transfer_ex'], 7);
   _pal_wasm_push_uart_rx_byte = Module['_pal_wasm_push_uart_rx_byte'] = createExportWrapper('pal_wasm_push_uart_rx_byte', wasmExports['pal_wasm_push_uart_rx_byte'], 2);
   _pal_wasm_report_oom = Module['_pal_wasm_report_oom'] = createExportWrapper('pal_wasm_report_oom', wasmExports['pal_wasm_report_oom'], 2);
   _pal_wasm_push_uart_rx_error = Module['_pal_wasm_push_uart_rx_error'] = createExportWrapper('pal_wasm_push_uart_rx_error', wasmExports['pal_wasm_push_uart_rx_error'], 2);
@@ -2723,6 +2733,8 @@ var wasmImports = {
   js_pal_gpio_write: _js_pal_gpio_write,
   /** @export */
   js_pal_i2c_transfer: _js_pal_i2c_transfer,
+  /** @export */
+  js_pal_i2c_transfer_ex: _js_pal_i2c_transfer_ex,
   /** @export */
   js_pal_log: _js_pal_log,
   /** @export */
